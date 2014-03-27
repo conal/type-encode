31af25b6c47993e3e04aa40a91019600b73717c2
Conal Elliott <conal@conal.net>
2014-03-26 19:35:14 -0700

    Examples (passing)

--- test/Test.hs
+++ test/Test.hs
@@ -21,11 +21,60 @@
 module Test where
 
 -- Needed for resolving names. Is there an alternative?
+import GHC.Tuple ()
+import Data.Either ()
 import qualified TypeEncode.Encode
+
+q :: Int
+q = 3
+
+t0 :: ()
+t0 = ()
 
 t1 :: Bool
 t1 = True
 
 t2 :: [Int]
 t2 = [1,2,3]
+
+t3 :: [Bool]
+t3 = [True,False]
+
+t4 :: (Int,Int)
+t4 = (3,4)
+
+t5 :: (Int,Int,Int,Int,Int)
+t5 = (3,4,5,6,7)
+
+data A = B Int | C Bool | Y Int | Z ()
+
+t6 :: A
+t6 = B 3
+
+data D = D
+
+t7 :: D
+t7 = D
+
+data E a = E a a
+
+t8 :: E Bool
+t8 = E False True
+
+t9 :: E ()
+t9 = E () ()
+
+fizzle :: String
+fizzle = "fizzle"
+
+newtype F a = F (a,a)
+
+-- The next two fail. Appartently callDataConT doesn't work for newtype
+-- constructors. Investigate.
+
+t10 :: F Bool
+t10 = F (False,True)
+
+t11 :: F ()
+t11 = F ((),())
 
